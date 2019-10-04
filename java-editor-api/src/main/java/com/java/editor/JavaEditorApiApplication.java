63b2d428e614131589e6709c67e80af92ecfe8fb
Anuj Kumar <anujgupta1403@gmail.com>
2019-10-05 03:46:57 +0530

    Added java editor API

---
+++ java-editor-api/src/main/java/com/java/editor/JavaEditorApiApplication.java
@@ -0,0 +1,13 @@
+package com.java.editor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEditorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaEditorApiApplication.class, args);
+	}
+
+}
